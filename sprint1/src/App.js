daf0c413c94a601252fba6022ebd43c27be71096
Romario477 <60476563+Romario477@users.noreply.github.com>
2021-06-23 22:52:05 -0400

    Popup

--- sprint1/src/App.js
+++ sprint1/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import ImageVerify from './Components/ImageVerify';
+import { useState } from 'react';
 function App() {
+    const [buttonPopup, setButtonPopup] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+          <main>
+              <h1>React Pop up</h1>
+              <br></br>
+              <button onClick={() => setButtonPopup(true)} > Open Popup</button>
+          </main>
+          <ImageVerify trigger={buttonPopup} setTrigger={setButtonPopup}>
+              <h3>Pop Up Message</h3>
+          </ImageVerify>
     </div>
   );
 }
