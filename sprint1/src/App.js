4edc2629cf217d32f3c70060ae647d1b54668c60
wpompa1 <77293165+wpompa1@users.noreply.github.com>
2021-07-09 20:24:36 -0400

    Update App.js
    Stuff

--- sprint1/src/App.js
+++ sprint1/src/App.js
@@ -5,45 +5,77 @@ import "./css/checkbox.css"
 import "bootstrap/dist/css/bootstrap.css"
 import { Button, Form, FormGroup } from "react-bootstrap"
 
-//This is a simple placeholder for the image randomizer we'll add. We'll delete this later, I just needed a visual outline to reflect on.
-/*function ImgList() {
+var usedImg = ["", "" , "" , "", "", "", "", "", ""];
+var index = 1;
+
+function ImgList() {
+    
+    //const keywords = ["Cat", "Pizza", "Car"];
+
     var images = [
-        {keyword : "Cat",
-         imglist : {
-            img1 : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaJVYJ3rT9KVeyvWltTFmfpv8C0CkhlQIQ4A&usqp=CAU"],
-            img2 : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-zpMzK7G6HwXUPNRU-BA03jx_1oC4Gl0NUg&usqp=CAU"],
-            img3 : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5woNyRoVaXNN_8rTxh9bmEGa0QxUCHqVsmg&usqp=CAU"]  
-         }
+        {keyword  : "Cat",
+         imglist  : [
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaJVYJ3rT9KVeyvWltTFmfpv8C0CkhlQIQ4A&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-zpMzK7G6HwXUPNRU-BA03jx_1oC4Gl0NUg&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5woNyRoVaXNN_8rTxh9bmEGa0QxUCHqVsmg&usqp=CAU"  
+         ]
         },
 
-        {keyword :"Pizza",
-         imglist : {
-            img4 : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz-eJmvQnM5YyC8UwvvIr56hMyQr0Wa6vppA&usqp=CAU"],
-            img5 : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyuIjca1eL6dRRtFlrrV-4JSOZzDVEzuqK7g&usqp=CAU"],
-            img6 : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfs32OMWj9HaCk9mrdlbvYTWt0yCKWGP4EnQ&usqp=CAU"]
-         }
+        {keyword  : "Pizza",
+         imglist  : [
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz-eJmvQnM5YyC8UwvvIr56hMyQr0Wa6vppA&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyuIjca1eL6dRRtFlrrV-4JSOZzDVEzuqK7g&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfs32OMWj9HaCk9mrdlbvYTWt0yCKWGP4EnQ&usqp=CAU" 
+         ]
         },
 
-        {keyword :"Car",
-         imglist : {
-            img7 : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQci4ViVbKJW1HbRq0H9BlZEYdTqSUcFeSkQA&usqp=CAU"],
-            img8 : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlBSI6p-YvPu4Sy_J-h_WfqcHudtYMr_pGoQ&usqp=CAU"],
-            img9 : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPvuyoxmr7oLHJzLhsioZXqmRI2G_F5WOXQ&usqp=CAU"]
-         }
+        {keyword  : "Car",
+         imglist  : [
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQci4ViVbKJW1HbRq0H9BlZEYdTqSUcFeSkQA&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlBSI6p-YvPu4Sy_J-h_WfqcHudtYMr_pGoQ&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPvuyoxmr7oLHJzLhsioZXqmRI2G_F5WOXQ&usqp=CAU"
+         ]
         }
-    ] 
+    ]; 
+
+     /*
+    I am trying to use a nested loop with a while loop on repeat until the next most available spot in the array is reached.
+    Inside the while loop is a for loop that compares the current keyword + image (using array indices, look at return())
+    to each entry in the array up to index - 1. If the for loop finds a match then the loop stops and the while loop begins
+    again with a new keyword + image. I have an if statement that stops the for loop and also sets the while loop to stop
+    if the next most available spot in the array is reach (should be index - 1, [used][next][index]). For some reason the 
+    nested loop causes my browser to freeze. I think there is an infinite loop, but it could be something else I.D.K.  
+    */
+
+    var exit = false;
+    //while(exit == false) {
+
+        var keyNum = Math.floor(Math.random() * 3); //Change this to return the Keyword String? images[Math.round(Math.random() * 3)].keyword.
+        var imgNum = Math.floor(Math.random() * 3);
+        
+       /* for(var i = 0; i < index; i++) {
+            var num = String(""+keyNum+""+imgNum).localeCompare(usedImg[i]);
+            if(num === 0) {
+                break;
+            }
+
+            if(i === index - 1) {
+                usedImg[i] = String(""+keyNum+""+imgNum);
+                index++;
+                exit = true;
+                console.log(usedImg[i]);
+            }
+        }*/
+
+            
+    //}
 
     return(
-        Math.round(Math.random() * 10) 
-
-        for (var i = 0, imagesLength = images.length  i < imagesLength  i++) {
-            for (var j = 0, imglistLength = images[i].imglist.length  j < imglistLength  j++) {
-            // do something with each list of each image keyword (i.e, for each cat picture do something)
-            }
-        }
+       
+        images[keyNum].imglist[imgNum]     
 
     )
-}*/
+}
 
 function AssignImages() {
   const imgs = {
@@ -51,41 +83,10 @@ function AssignImages() {
     height: "84px",
   }
 
-  const keywords = ["Cat", "Pizza", "Car"]
-
-  var images = [
-    {
-      keyword: "Cat",
-      imglist: [
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaJVYJ3rT9KVeyvWltTFmfpv8C0CkhlQIQ4A&usqp=CAU",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-zpMzK7G6HwXUPNRU-BA03jx_1oC4Gl0NUg&usqp=CAU",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5woNyRoVaXNN_8rTxh9bmEGa0QxUCHqVsmg&usqp=CAU",
-      ],
-    },
-
-    {
-      keyword: "Pizza",
-      imglist: [
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz-eJmvQnM5YyC8UwvvIr56hMyQr0Wa6vppA&usqp=CAU",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyuIjca1eL6dRRtFlrrV-4JSOZzDVEzuqK7g&usqp=CAU",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfs32OMWj9HaCk9mrdlbvYTWt0yCKWGP4EnQ&usqp=CAU",
-      ],
-    },
-
-    {
-      keyword: "Car",
-      imglist: [
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQci4ViVbKJW1HbRq0H9BlZEYdTqSUcFeSkQA&usqp=CAU",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlBSI6p-YvPu4Sy_J-h_WfqcHudtYMr_pGoQ&usqp=CAU",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPvuyoxmr7oLHJzLhsioZXqmRI2G_F5WOXQ&usqp=CAU",
-      ],
-    },
-  ]
-
   //function I was creating to add images onto the grid
 
   return (
-    <img style={imgs} className="imgs" src={images[0].imglist[0]} alt="logo" />
+    <img style={imgs} className="imgs" src={ImgList()} alt="logo" />
   )
 }
 
