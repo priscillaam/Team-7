ce5b863c5b6674a7c5dfc43d209148d7428980f8
Priscilla Martinez <60373527+priscillaam@users.noreply.github.com>
2021-07-10 13:12:12 -0400

    Rearranging Files

--- sprint1/src/Components/ImageVerify.js
+++ sprint1/src/Components/ImageVerify.js
@@ -2,19 +2,50 @@ import React from "react"
 import "./ImageVerify.css"
 //npm install react-image --save
 import { useImage } from "react-image"
+import Grid from './Grid.js'
 
 function ImageVerify(props) {
+  const popup = {
+    position: "fixed",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "100vh",
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  }
+
+  const popupInner = {
+    position: "relative",
+    padding: "32px",
+    width: "100%",
+    maxWidth: "600px",
+    backgroundColor: "#FFF",
+    top: "16px",
+    right: "16px",
+  }
+
   return props.trigger ? (
-    <div className="popup">
-      <div className="popup-inner">
-        <button className="close-btn" onClick={() => props.setTrigger(false)}>
-          Close
+    <div style={popup}>
+      <div style={popupInner}>
+        <button
+          style={{ position: "absolute", top: "16px", right: "16px" }}
+          onClick={() => props.setTrigger(false)}
+        >
+          x
         </button>
         {props.children}
-        <img
-          src="https://icatcare.org/app/uploads/2018/07/Helping-your-new-cat-or-kitten-settle-in-1.png"
-          alt="No Image"
-        />
+        {/* <img src="https://icatcare.org/app/uploads/2018/07/Helping-your-new-cat-or-kitten-settle-in-1.png" alt="No Image" /> */}
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Grid />
+        </div>
       </div>
     </div>
   ) : (
