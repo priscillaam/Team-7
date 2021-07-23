4f6c24419290aac8c65fe1ca6b45c564e5b8bd8e
Priscilla Martinez <60373527+priscillaam@users.noreply.github.com>
2021-07-23 16:42:27 -0400

    Login
    Changed some things for login to make it look like a dummy website

--- sprint1/src/App.js
+++ sprint1/src/App.js
@@ -9,37 +9,56 @@ import { Button, Form, FormGroup } from "react-bootstrap"
 function App() {
   const [buttonPopup, setButtonPopup] = useState(false)
 
+  const div = {
+    width: "425px",
+    height: "500px",
+    backgroundColor: "white",
+    position: "relative",
+    padding: "32px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  }
   return (
-    <div className="App">
+    <div>
       <main>
-        <h1>Sign In</h1>
-        <br></br>
+        <div class="background"></div>
+        <div style={div}>
+          <h1>Sign In</h1>
+          <br></br>
 
-        <Form>
-          <FormGroup controlId="formUsername">
-            <Form.Label>Username</Form.Label>{" "}
-            <Form.Control
-              id="username"
-              type="text"
-              placeholder="Username"
-            ></Form.Control>
-          </FormGroup>
-          <br />
-          <FormGroup controlId="formPassword">
-            <Form.Label>Password</Form.Label>{" "}
-            <Form.Control type="password" placeholder="Password"></Form.Control>
-            <Form.Text>Feel safe logging in using our secure forms.</Form.Text>
-          </FormGroup>
-        </Form>
-        <br></br>
-        <Button
-          variant="secondary"
-          type="submit"
-          onClick={() => setButtonPopup(true)}
-        >
-          {" "}
-          Login
-        </Button>
+          <Form>
+            <FormGroup controlId="formUsername">
+              <Form.Label>Username</Form.Label>{" "}
+              <Form.Control
+                id="username"
+                type="text"
+                placeholder="Username"
+              ></Form.Control>
+            </FormGroup>
+            <br />
+            <FormGroup controlId="formPassword">
+              <Form.Label>Password</Form.Label>{" "}
+              <Form.Control
+                type="password"
+                placeholder="Password"
+              ></Form.Control>
+              <Form.Text>
+                Feel safe logging in using our secure forms.
+              </Form.Text>
+            </FormGroup>
+          </Form>
+          <br></br>
+          <Button
+            variant="secondary"
+            type="submit"
+            onClick={() => setButtonPopup(true)}
+          >
+            {" "}
+            Login
+          </Button>
+        </div>
       </main>
 
       <ImageVerify
